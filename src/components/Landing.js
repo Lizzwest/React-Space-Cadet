ea0b724a76fec4a88579fcff757bedd6901a9825
Lizz <lizwesterband@gmail.com>
2020-10-14 22:40:11 -0700

    add stubbed landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Landing = () => {
+    let [username, setUsername] = useState('');
+    
+    let handleUsername = (e) => {
+        setUsername(e.target.value);
+    }
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    const cadetData= { username }
     return (
-        <div className="landingBody">
-      <h2>Welcome</h2>
-      <h1> Space Cadet </h1>
+    <div className="landingBody">
+      <h2 className="welcome">Welcome</h2>
+      <h1 className="spaceCadet"> Space Cadet </h1>
+      <div className="astronaut">
+     <div>
+         <img src={'https://res-console.cloudinary.com/dl7aojigh/thumbnails/v1/image/upload/v1602738390/YXN0cm9uYXV0X2xhbmRpbmdfcGFnZV9pbWFnZV9sOXl0c2E=/preview'} alt="astronaut" className="astronaut"/></div>
+      </div>
+      <h4 className="labelForUsername"> What should we call you?</h4>
+      <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label><br />
+                            <input type="text" name="username" value={username} onChange={handleUsername} className="usernameInput" required />
+                        </div>
+      </form>
+      <button className="submitUsername" type='submit'>Start Mission</button>
       
       
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { NavLink, Link } from 'react-router-dom';
 const Navbar = (props) => {
     return (
     
-        <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "#490066"}}>
+        <nav className="navbar navbar-dark">
             <div className="container">
                 <NavLink className="landingLink" to="/">Landing Icon</NavLink>
                 <NavLink className="nav-link" exact to="/logout">Exit</NavLink>
